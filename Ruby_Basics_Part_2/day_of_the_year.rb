ad6293ced0428ae74c9fee293265c443672ce6c0
kaltag <alexx30rus@gmail.com>
2023-03-15 10:08:52 +0400

    fixed bug with counting days

--- Ruby_Basics_Part_2/day_of_the_year.rb
+++ Ruby_Basics_Part_2/day_of_the_year.rb
@@ -5,12 +5,14 @@ month = gets.chomp.to_i
 puts 'year?'
 year = gets.chomp.to_i
 
-all_days = 1
+all_days = 0
 arr_Month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 (1..month - 1).each do |i|
   all_days += arr_Month[i]
 end
+
+all_days += day
 
 all_days += 1 if month > 2 && year % 4 == 0 && !(year % 100 == 0 && year % 400 != 0)
 
